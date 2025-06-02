633ed80ab5897efdf7b8a78d6d5652986d6d2160
dasjero98772 <jero98772@gmail.com>
2025-06-01 23:07:19 -0500

    fixed code style

--- main.py
+++ main.py
@@ -5,13 +5,7 @@ def listdir_without_hidden(path):
     for file in os.listdir(path):
         if not file.startswith('.'):
             yield file
-
-
-def back(file_path):
-    parts = file_path.split('/')
-    new_path = '/'.join(parts[:-1])
-    return new_path
-
+            
 @app.route('/')
 @app.route('/<path:path>')
 def index(path=None):
@@ -21,15 +15,10 @@ def index(path=None):
         pass
     else:
         path="/"+path
-    print("1",path)
-    previus=back(path)
-    print("previus",previus)
-    print("path",path)
     data = list(listdir_without_hidden(path))
-    print(list(data))
     currentDirectory = os.path.abspath(path)
     
-    return render_template('index.html', data_array=data, currentDirectory=path,previus=previus)
+    return render_template('index.html', data_array=data)
 
 
 if __name__ == '__main__':
